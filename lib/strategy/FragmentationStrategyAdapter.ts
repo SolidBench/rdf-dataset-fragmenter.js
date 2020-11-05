3d9e6e20702665f3267e8bc845ca1908249c951e
Ruben Taelman <ruben.taelman@ugent.be>
2020-11-05 13:18:58 +0100

    Add fragmentation strategy

---
+++ lib/strategy/FragmentationStrategyAdapter.ts
@@ -0,0 +1,39 @@
+import type { Readable, TransformCallback } from 'stream';
+import { Transform } from 'stream';
+import type * as RDF from 'rdf-js';
+import type { IQuadSink } from '../io/IQuadSink';
+import type { IFragmentationStrategy } from './IFragmentationStrategy';
+
+/**
+ * An abstract implementation of a fragmentation strategy.
+ */
+export abstract class FragmentationStrategyAdapter implements IFragmentationStrategy {
+  public async fragment(quadStream: RDF.Stream & Readable, quadSink: IQuadSink): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias,consistent-this
+    const self = this;
+    const transform = new Transform({
+      objectMode: true,
+      // eslint-disable-next-line no-undef
+      transform(quad: RDF.Quad, encoding: BufferEncoding, callback: TransformCallback) {
+        self.handleQuad(quad, quadSink).then(() => callback(), callback);
+      },
+    });
+
+    await new Promise((resolve, reject) => {
+      quadStream.on('error', error => transformed.emit('error', error));
+      const transformed = quadStream.pipe(transform);
+      // Enter flow-mode, but not need to attach a data-listener
+      transformed.resume();
+      transformed.on('error', reject);
+      transformed.on('end', resolve);
+    });
+
+    await this.flush(quadSink);
+  }
+
+  protected abstract handleQuad(quad: RDF.Quad, quadSink: IQuadSink): Promise<void>;
+
+  protected async flush(quadSink: IQuadSink): Promise<void> {
+    // Do nothing, implementors of this class can optionally override this
+  }
+}
