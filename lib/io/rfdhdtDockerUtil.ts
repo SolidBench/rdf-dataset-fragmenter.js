9c9f93af661fabfc830c8acbd1c66e43b5f7adaf
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-06-13 15:22:26 +0200

    lint fix

--- lib/io/rfdhdtDockerUtil.ts
+++ lib/io/rfdhdtDockerUtil.ts
@@ -1,7 +1,15 @@
-import * as Docker from 'dockerode';
-import * as Path from "path";
+import * as Path from 'path';
+import type * as Docker from 'dockerode';
 
-const RDF_HDT_IMAGE_REPO = "rfdhdt/hdt-cpp";
+const SUPPORTED_FORMAT = new Map([
+  [ '.nq', 'nquad' ],
+  [ '.nt', 'ntriples' ],
+  [ '.ttl', 'turtle' ],
+  [ '.xml', 'rdfxml' ],
+  [ '.n3', 'n3' ],
+]);
+
+const RDF_HDT_IMAGE_REPO = 'rfdhdt/hdt-cpp';
 
 /**
  * Pull the rfdhdt/hdt-cpp with docker
@@ -9,76 +17,68 @@ const RDF_HDT_IMAGE_REPO = "rfdhdt/hdt-cpp";
  * docker pull rfdhdt/hdt-cpp
  */
 export async function pullHdtCppDockerImage(docker: Docker): Promise<void> {
-    return new Promise(async (resolve, reject) => {
+  return new Promise(async(resolve, reject) => {
+    const stream = await docker.pull(RDF_HDT_IMAGE_REPO, {});
 
-        const stream = await docker.pull(RDF_HDT_IMAGE_REPO, {});
+    const onFinished = (err: any): undefined => {
+      if (err === undefined || err === null) {
+        resolve();
+      } else {
+        // eslint-disable-next-line no-console
+        console.info(err);
+        reject(err);
+      }
+    };
 
-        const onFinished = (err: any) => {
-            if (err === undefined || err === null) {
-                resolve();
-            } else {
-                console.info(err);
-                reject(err);
-            }
-        }
+    const onProgress = (event: any): undefined => {
+      // eslint-disable-next-line no-console
+      console.info(event);
+    };
 
-        const onProgress = (event: any) => {
-            console.info(event);
-        }
-
-        docker.modem.followProgress(stream, onFinished, onProgress);
-
-    });
+    docker.modem.followProgress(stream, onFinished, onProgress);
+  });
 }
 
 /**
- * convert an ntriple document into an HDT using the rfdhdt/hdt-cpp docker image
+ * Convert an ntriple document into an HDT using the rfdhdt/hdt-cpp docker image
  * Equivalent to the docker command
- * docker run -it --rm -v "$(pwd)":{inputFileFolder} rfdhdt/hdt-cpp rdf2hdt -i -p -v -f ntriples {inputFilePath} {outputFilePath}
- * @param {string} inputFilePath -the path of the file to convert 
+ * docker run -it --rm -v "$(pwd)":{inputFileFolder} rfdhdt/hdt-cpp /
+ * rdf2hdt -i -p -v -f ntriples {inputFilePath} {outputFilePath}
+ * @param {string} inputFilePath -the path of the file to convert
  */
 export async function convertToHdt(docker: Docker, inputFilePath: string): Promise<void> {
-    const parsedPath = Path.parse(inputFilePath);
+  const parsedPath = Path.parse(inputFilePath);
 
-    const outputFolder = parsedPath.dir;
-    const filename = parsedPath.name;
-    const fileExtension = parsedPath.ext;
+  const outputFolder = parsedPath.dir;
+  const filename = parsedPath.name;
+  const fileExtension = parsedPath.ext;
 
-    const format = SUPPORTED_FORMAT.get(fileExtension);
-    if (format === undefined) {
-        throw new Error(`format ${fileExtension} not support by rfdhdt/hdt-cpp`);
-    }
-    const command = [
-        "rdf2hdt",
-        "-i",
-        "-p",
-        "-v",
-        "-f",
-        format,
-        Path.resolve(inputFilePath),
-        Path.resolve(Path.join(outputFolder, `${filename}.hdt`))
-    ];
-    console.log(Path.resolve(inputFilePath));
-    const createOption: Docker.ContainerCreateOptions = {
-        HostConfig: {
-            AutoRemove: true,
-            Binds: [
-                `${process.cwd()}:${Path.resolve("./")}`
-            ]
-        },
-        Tty:false
-    };
-    const data = await docker.run(RDF_HDT_IMAGE_REPO, command, [process.stdout, process.stderr], createOption);
-    const [output] = data;
-    if (output.StatusCode===1){
-        throw new Error("was not able to finish the task. Check the terminal log for more information.")
-    }
+  const format = SUPPORTED_FORMAT.get(fileExtension);
+  if (format === undefined) {
+    throw new Error(`format ${fileExtension} not support by rfdhdt/hdt-cpp`);
+  }
+  const command = [
+    'rdf2hdt',
+    '-i',
+    '-p',
+    '-v',
+    '-f',
+    format,
+    Path.resolve(inputFilePath),
+    Path.resolve(Path.join(outputFolder, `${filename}.hdt`)),
+  ];
+  const createOption: Docker.ContainerCreateOptions = {
+    HostConfig: {
+      AutoRemove: true,
+      Binds: [
+        `${process.cwd()}:${Path.resolve('./')}`,
+      ],
+    },
+    Tty: false,
+  };
+  const data = await docker.run(RDF_HDT_IMAGE_REPO, command, [ process.stdout, process.stderr ], createOption);
+  const [ output ] = data;
+  if (output.StatusCode === 1) {
+    throw new Error('was not able to finish the task. Check the terminal log for more information.');
+  }
 }
-
-const SUPPORTED_FORMAT = new Map([
-    ['.nq', 'nquad'],
-    ['.nt', 'ntriples'],
-    ['.ttl', 'turtle'],
-    ['.xml', 'rdfxml'],
-    ['.n3', 'n3']
-]);
