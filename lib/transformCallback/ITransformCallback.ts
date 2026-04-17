9c5fcbe662f62b275e4480d30fe0f2bd801177c0
RubenEschauzier <ruben.eschauzier@gmail.com>
2026-04-17 09:42:44 +0200

    Add a small docstring

--- lib/transformCallback/ITransformCallback.ts
+++ lib/transformCallback/ITransformCallback.ts
@@ -1,5 +1,9 @@
 import type * as RDF from '@rdfjs/types';
 
+/**
+ * Callback after the transform function, to do post-transform operations,
+ * such as writing the original and transformed quads to file.
+ */
 export interface ITransformCallback {
   run: (quad: RDF.Quad, transformedQuads: RDF.Quad[]) => Promise<void>;
   initializeCallback: () => Promise<void>;
